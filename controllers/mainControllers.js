006ad14fa4c4037ace87d25f9c9011902a997b2d
Ahmad Yosry <87082240+Gonksy@users.noreply.github.com>
2024-03-24 14:15:54 +0200

    getDash and getUsers get users and return appropriate responses

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,10 +1,20 @@
-const users = require("../users.json")
+// const users = require("../users.json")
+const { fetchUsers } = require("../services/dataServices")
 
 module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs")
   },
   getDash: (req, res) => {
+    // Responds to dashboard requests
+    const users = fetchUsers(req).users
     res.render("dash.ejs", { users: users })
+  },
+  getUsers: (req, res) => {
+    // Responds to API calls
+    const response = fetchUsers(req)
+    const status = response.status
+    const payload = response.users || response.error
+    return res.status(status).json(payload)
   },
 }
